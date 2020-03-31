5a0c176be4b4fe1759f1208c64d9e268e7f0e738
joking <250348426@qq.com>
2020-03-31 18:03:58 +0800

    cg

--- src/test-command-params.js
+++ src/test-command-params.js
@@ -1,6 +1,6 @@
 const vscode = require('vscode');
 module.exports = function(context) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.demo.getCurrentFilePath', (uri) => {
-        vscode.window.showInformationMessage(`当前文件(夹)路径是：${uri ? uri.path : '空'}`);
+        vscode.window.showInformationMessage(`当前文件(夹)路径是：${uri ? uri.path : '这个我咋知道呢'}`);
     }));
 };
